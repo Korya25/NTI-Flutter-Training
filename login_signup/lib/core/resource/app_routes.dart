036da6af232aa5080930bc02dfa1e52f18bc11ca
Korya25 <????? ??? GitHub>
2025-06-25 20:15:29 +0300

    delate login and sign up

--- login_signup/lib/core/resource/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const String login = '/login';
-  static const String register = '/register';
-  static const String profile = '/profile';
-  static const String allUsers = '/all-users';
-  static const String userDetail = '/user-detail';
-
-}
